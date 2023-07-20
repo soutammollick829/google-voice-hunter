d1746b73abcec38321f5dd97c93efc3757712c94
soutammolick829 <soutammollick829@gmail.com>
2023-07-20 21:59:42 +0600

    added link social media

--- src/Shared page/Footer/Footer.jsx
+++ src/Shared page/Footer/Footer.jsx
@@ -9,21 +9,29 @@ import paymentLogo2 from "../../assets/Payment/307737508_468589261952791_4326392
 import paymentLogo3 from "../../assets/Payment/tether-usdt-vpn-trustzone.png";
 
 import Logo from "../../assets/Logo/buy-google-voice-accounts.webp";
+import qrcode from "../../assets/Logo/362293611_596150325997627_n.jpg";
 
 const Footer = () => {
   return (
     <div>
       <footer className="footer p-10 bg-base-200 text-base-content">
-        <div className="space-y-5">
+        <div className="space-y-2">
           <span className="footer-title">24/7 Live support</span>
-          <h3 className="text-2xl text-cyan-500">
-            <FaRegPaperPlane />
+          <h3 className="inline-flex items-center text-base font-bold text-[#00796B] hover:text-[#689f38]">
+            <FaWhatsappSquare className="text-4xl text-green-600" /> :
+            +8801611975125
           </h3>
-          <h3>
-            <FaWhatsappSquare className="text-2xl text-green-600" />
+          <h3 className="text-base font-bold text-[#00796B] hover:text-[#689f38] inline-flex">
+            <a href="https://t.me/GoogleVoiceHunter" target="blank">
+              <FaRegPaperPlane className="text-cyan-500 text-4xl" />
+            </a>
+            @GoogleVoiceHunter
           </h3>
+
           <h3>
-            <FaFacebook className="text-2xl text-blue-600" />
+            <a href="https://www.facebook.com/profile.php?id=100029163254747&mibextid=ZbWKwL">
+              <FaFacebook className="text-4xl text-blue-600" />
+            </a>
           </h3>
         </div>
         <div>
@@ -36,7 +44,9 @@ const Footer = () => {
                 alt="image"
               />
               <div className="text-base font-bold">
-                <h1 className="text-[#00796B] hover:text-[#689f38]">Text Now Messenger ID</h1>
+                <h1 className="text-[#00796B] hover:text-[#689f38]">
+                  Text Now Messenger ID
+                </h1>
                 <p>$ 1.00</p>
               </div>
             </div>
@@ -47,7 +57,9 @@ const Footer = () => {
                 alt="image"
               />
               <div className="text-base font-bold">
-                <h1 className="text-[#00796B] hover:text-[#689f38]">Google voice ID</h1>
+                <h1 className="text-[#00796B] hover:text-[#689f38]">
+                  Google voice ID
+                </h1>
                 <p>$ 5.00</p>
               </div>
             </div>
@@ -58,7 +70,9 @@ const Footer = () => {
                 alt="image"
               />
               <div className="text-base font-bold">
-                <h1 className="text-[#00796B] hover:text-[#689f38]">Chat Gpt ID</h1>
+                <h1 className="text-[#00796B] hover:text-[#689f38]">
+                  Chat Gpt ID
+                </h1>
                 <p>$ 2.00</p>
               </div>
             </div>
@@ -69,7 +83,9 @@ const Footer = () => {
                 alt="image"
               />
               <div className="text-base font-bold">
-                <h1 className="text-[#00796B] hover:text-[#689f38]">USA Gmail</h1>
+                <h1 className="text-[#00796B] hover:text-[#689f38]">
+                  USA Gmail
+                </h1>
                 <p>$ 1.00</p>
               </div>
             </div>
@@ -84,7 +100,7 @@ const Footer = () => {
           </div>
         </div>
       </footer>
-      <footer className="footer px-10 py-4 border-t bg-[#689f38] text-base-content border-base-300">
+      <footer className="footer px-10 py-4 border-t bg-[#689f38] text-base-content border-base-300 grid-flow-col">
         <div className="items-center grid-flow-col">
           <img className="w-28 rounded-full" src={Logo} alt="logo" />
           <p>
@@ -94,39 +110,7 @@ const Footer = () => {
         </div>
         <div className="md:place-self-center md:justify-self-end">
           <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
+            <img className="w-16 rounded-lg" src={qrcode} alt="" />
           </div>
         </div>
       </footer>
--- src/Shared page/Navbar/Logobar/Logobar.jsx
+++ src/Shared page/Navbar/Logobar/Logobar.jsx
@@ -1,28 +1,50 @@
 import logo from "../../../assets/Logo/Google_Voice_icon_(2018-2020).svg.png";
-import { FaFacebook, FaRegEnvelope, FaShoppingBasket, FaSkype, FaWhatsappSquare} from 'react-icons/fa';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaRegEnvelope,
+  FaRegPaperPlane,
+  FaShoppingBasket,
+  FaWhatsappSquare,
+} from "react-icons/fa";
 const Logobar = () => {
   return (
     <div className="lg:flex justify-around items-center mt-5 space-y-5">
       <div className="flex items-center gap-3 ml-10 lg:ml-0">
         <img className="w-16" src={logo} alt="logo" />
         <h2 className="text-4xl font-bold text-[#00796B]">
-          Google voice <br /> <span className="text-lg uppercase ml-16 text-[#00796B]">H u n t e r</span>
+          Google voice <br />
+          <span className="text-lg uppercase ml-16 text-[#00796B]">
+            H u n t e r
+          </span>
         </h2>
       </div>
-      <div className="lg:flex gap-6 ml-28 lg:ml-0 space-y-5 lg:space-y-0">
+      <div className="lg:flex gap-4 ml-28 lg:ml-0 space-y-5 lg:space-y-0">
         <div>
-            <h2 className="flex gap-2 items-center"><FaWhatsappSquare className="text-2xl text-green-600"/> +8801911666960</h2>
+          <h2 className="flex gap-2 items-center">
+            <FaWhatsappSquare className="text-2xl text-green-600" />{" "}
+            +8801611975125
+          </h2>
         </div>
         <div>
-            <h2 className="flex gap-2 items-center "><FaRegEnvelope className="text-2xl text-red-400"/> rajumandal@gmail.com</h2>
+          <h2 className="flex gap-2 items-center ">
+            <FaRegEnvelope className="text-2xl text-red-400" />{" "}
+            rajuofficial247@gmail.com
+          </h2>
         </div>
-        <div className="inline-flex gap-3 text-2xl">
-            <FaFacebook className="text-blue-600"/> <FaSkype className="text-sky-500"/>
-        </div>
+        <div className="inline-flex gap-2 mt-2 text-3xl">
+            <a href="https://www.facebook.com/profile.php?id=100029163254747&mibextid=ZbWKwL" target="blank"><FaFacebook className="text-blue-600" /></a>
+            <a href="https://t.me/GoogleVoiceHunter" target="blank"><FaRegPaperPlane className="text-cyan-500" /></a>
+            <a href="https://instagram.com/nguraju?igshid=ZDc4ODBmNjlmNQ=="><FaInstagram className="text-red-800" target="blank"/></a>
+          </div>
       </div>
       <div className="relative ml-24 lg:ml-0">
-        <button className="btn rounded-none"><FaShoppingBasket className="text-[#00796B] text-xl"/> My cart</button>
-        <div className="badge bg-[#00796B] absolute -top-2 right-44 lg:-right-1 text-white font-bold">+99</div>
+        <button className="btn rounded-none">
+          <FaShoppingBasket className="text-[#00796B] text-xl" /> My cart
+        </button>
+        <div className="badge bg-[#00796B] absolute -top-2 right-44 lg:-right-1 text-white font-bold">
+          +99
+        </div>
       </div>
     </div>
   );
